db801bd4e45a0f9f03916ed19ce26745bff7f0c2
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2023-09-22 21:48:23 +0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
-function App() {
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-  return (
-    <div>Hello World</div>
-  )
+import "./styles/global.scss";
+
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            {/* <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider> */}
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+        // {
+        //   path: "/users/:id",
+        //   element: <User />,
+        // },
+        // {
+        //   path: "/products/:id",
+        //   element: <Product />,
+        // },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
